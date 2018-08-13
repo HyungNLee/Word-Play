74c3f04f2b768f3f7ae6665cc99d7e10100ea8c9
David Zhu and Hyung Lee <david@>
2018-08-13 15:17:52 -0700

    Refactor JS code.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,23 @@ $(document).ready(function() {
     var userInput = $("input#inputBox").val();
     var initialArray = userInput.split(" ");
     var finalString = "";
-    var secondArray = [];
 
-    initialArray.forEach(function(word) {
-      if (word.length >= 3) {
-        word = word.split('.').join(' ');
-        secondArray.push(word);
-      }
+    var secondArray = initialArray.filter(word => word.length >= 3);
+    secondArray = secondArray.map(function(word) {
+      // word = word.split('.').join(' ');
+      return word.replace(".", "");
+      // return word;
     })
+
+    console.log(secondArray);
+
+
+    // initialArray.forEach(function(word) {
+    //   if (word.length >= 3) {
+    //     word = word.split('.').join(' ');
+    //     secondArray.push(word);
+    //   }
+    // })
 
     secondArray.reverse();
 
